1ac9801d87b44de9a078767b599fecfebf4ca8b9
Ali Saidik <saida130.210@gmail.com>
2019-10-18 17:53:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,5 +5,34 @@ def map_to_negativize(arr)
 end
 
 def map_to_no_change(arr)
-  arr.map
+  
+  
+def map_to_double(source_array)
+  double_array = []
+  i = 0 
+  while i < source_array.count do
+    double_array.push(source_array[i] * 2)
+    i += 1
+  end
+double_array
+end
+
+def map_to_square(source_array)
+  square_array = []
+  i = 0 
+  while i < source_array.count do
+    square_array.push(source_array[i] ** 2)
+    i += 1
+  end
+square_array
+end
+
+def reduce_to_total(source_array, starting_point=0)
+  total = starting_point
+  i = 0 
+  while i < source_array.count do
+    total += source_array[i]
+    i += 1
+  end
+total
 end
